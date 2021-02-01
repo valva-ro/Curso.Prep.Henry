261bfc44d8fdd2da563b9b83d39e622b2f87d4b5
Valentina Varela <vavarela.rodriguez@gmail.com>
2021-02-01 09:45:47 -0300

    refactor

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -184,11 +184,12 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
-  var limite = numero + 35;
+/*   var limite = numero + 35;
   do {
     numero += 5;
   } while (numero <= limite);
-  return numero;
+  return numero; */
+  return numero + 40;
 }
 
 
